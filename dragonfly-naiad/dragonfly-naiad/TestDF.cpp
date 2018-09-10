1cadeed38e90d4d8024b335fdf3d37109ee88568
March, Harrison Seung-Min <hsmarch@wpi.edu>
2018-09-10 15:31:29 -0400

    partial refactoring of naiad test

--- dragonfly-naiad/dragonfly-naiad/TestDF.cpp
+++ dragonfly-naiad/dragonfly-naiad/TestDF.cpp
@@ -15,157 +15,136 @@
 #include <Windows.h>
 
 int main(int argc, char * argv[]);
-void testLogManager();
-void testManager();
-void testClock();
-void testObject();
-void testObjectList();
-void testWorldManager();
-void testGameManager();
+void test_log_manager();
+void test_clock();
+void test_object();
+void test_object_list();
+void test_world_manager();
+void test_game_manager();
 
 //Global variables for test cases
-df::GameManager &g_mngr = df::GameManager::getInstance();
-df::LogManager &l_mngr = df::LogManager::getInstance();
-df::Object *testObj = new df::Object();
+df::GameManager &game_manager = df::GameManager::getInstance();
+df::LogManager &log_manager = df::LogManager::getInstance();
+df::Object *test_obj = new df::Object();
 Vector *test_position = new Vector();
 ObjectList *test_list = new ObjectList();
-df::Object *testObj2 = new df::Object();
+df::Object *test_obj2 = new df::Object();
 
 int main(int argc, char *argv[]) {
-
-	l_mngr.writeLog(" - Starting Test Session - \n");
-	testLogManager();
-	testManager();
-	testClock();
-	testObject();
-	testObjectList();
-	testWorldManager();
-	testGameManager();
+	test_log_manager();
+	test_clock();
+	test_object();
+	test_object_list();
+	test_world_manager();
+	test_game_manager();
 }
-
-void testLogManager()
+/**
+ *Log Manager Tests.
+ *- Checks if flush is enabled
+ *- Checks manager.getType(
+ *- Checks printing, single and multiple args
+ */
+void test_log_manager()
 {
-	/*
-
-	Log Manager Tests.
-
-	- Checks if flush is enabled.
-	- Checks manager.getType()
-	- Checks printing, single and multiple args
-
-	*/
-	l_mngr.writeLog("Initializing Log Manager Tests. GM and LM init Success!\n");
-	if (l_mngr.do_flush) {
-		l_mngr.writeLog("LM Flush enabled. This message should not print.");
+	log_manager.writeLog(" - Starting Test Session - \n");
+	log_manager.writeLog("Initializing Log Manager Tests. GM and LM init Success!\n");
+	if (log_manager.do_flush) {
+		log_manager.writeLog("LM Flush enabled. This message should not print.");
 	}
-	l_mngr.writeLog("%s MultiArg Test %s %d\n", l_mngr.getType().c_str(), "qwerty", 3);
+	log_manager.writeLog("%s MultiArg Test %s %d\n", log_manager.getType().c_str(), "qwerty", 3);
 }
 
-void testManager()
-{
-	/*
-	Manager Tests.
-	 - Checks if manager can start
-	 - Checks default manager type
-	 - Shuts down manager
-	*/
-	l_mngr.writeLog("\n\n - Manager Test Suite -  \n");
-	df::Manager *manager = new df::Manager();
-	manager->startUp();
-	l_mngr.writeLog("Manager getType() Test: %s\n", manager->getType().c_str());
-	manager->shutDown();
-}
 
-void testClock()
+void test_clock()
 {
 	/*
 	Clock Tests.
 	 - Checks Split and delta methods of clock class.
 	 */
-	l_mngr.writeLog("\n\n - Clock Test Suite -  \n");
+	log_manager.writeLog("\n\n - Clock Test Suite -  \n");
 	Clock *test_clock = new Clock();
 	Sleep(1);
 	long int test_time = test_clock->split();
-	l_mngr.writeLog("\tTest: Clock Split %lu\n", test_time);
+	log_manager.writeLog("\tTest: Clock Split %lu\n", test_time);
 	Sleep(2);
 	test_time = test_clock->delta();
-	l_mngr.writeLog("\tTest: Clock Delta  %lu\n", test_time);
+	log_manager.writeLog("\tTest: Clock Delta  %lu\n", test_time);
 }
 
-void testObject()
+void test_object()
 {
 	/*
 	Object Tests
 	 - Creates an object
 	 - Sets params
 	 */
-	l_mngr.writeLog("\n\n - Object Test Suite - \n");
+	log_manager.writeLog("\n\n - Object Test Suite - \n");
 	
-	testObj->setType("Test Object");
-	testObj->setId(5);
+	test_obj->setType("Test Object");
+	test_obj->setId(5);
 	test_position->setXY(5, 4);
-	testObj->setPosition(*test_position);
-	l_mngr.writeLog("\t Object type: %s\n\t Object id: %d\n\t Object position: x=%d, y=%d\n", testObj->getType().c_str(), testObj->getId(), testObj->getPosition().getX(), testObj->getPosition().getY());
+	test_obj->setPosition(*test_position);
+	log_manager.writeLog("\t Object type: %s\n\t Object id: %d\n\t Object position: x=%d, y=%d\n", test_obj->getType().c_str(), test_obj->getId(), test_obj->getPosition().getX(), test_obj->getPosition().getY());
 
 }
 
-void testObjectList()
+void test_object_list()
 {
 	/*
 	ObjectList and ObjectListIterator Tests
 	 - Checks various object list and iterator features
 	 */
-	l_mngr.writeLog("\n\n - ObjectList and ObjectListIterator Test Suite - \n");
-	l_mngr.writeLog("\tList size: %d\n\tAdding to list\n", test_list->getCount());
-	testObj2->setType("Test Object 2");
-	test_list->insert(testObj);
-	test_list->insert(testObj2);
+	log_manager.writeLog("\n\n - ObjectList and ObjectListIterator Test Suite - \n");
+	log_manager.writeLog("\tList size: %d\n\tAdding to list\n", test_list->getCount());
+	test_obj2->setType("Test Object 2");
+	test_list->insert(test_obj);
+	test_list->insert(test_obj2);
 	for (int i = 0; i < 5001; i++) {
-		if (test_list->insert(testObj2) == 1) {
-			l_mngr.writeLog("\tObject List Full! Cannot create!\n");
+		if (test_list->insert(test_obj2) == 1) {
+			log_manager.writeLog("\tObject List Full! Cannot create!\n");
 		}
 	}
-	l_mngr.writeLog("\tAfter insertion: getCount(): %d\tisEmpty(): %d\n", test_list->getCount(), test_list->isEmpty());
+	log_manager.writeLog("\tAfter insertion: getCount(): %d\tisEmpty(): %d\n", test_list->getCount(), test_list->isEmpty());
 	for (int i = 0; i < 4995; i++) {
-		test_list->remove(testObj2);
+		test_list->remove(test_obj2);
 	}
-	l_mngr.writeLog("\tAfter deletion: getCount(): %d\tisEmpty(): %d\n", test_list->getCount(), test_list->isEmpty());
-	test_list->insert(testObj2);
-	test_list->insert(testObj2);
+	log_manager.writeLog("\tAfter deletion: getCount(): %d\tisEmpty(): %d\n", test_list->getCount(), test_list->isEmpty());
+	test_list->insert(test_obj2);
+	test_list->insert(test_obj2);
 	ObjectListIterator test_li(test_list);
-	l_mngr.writeLog("\tPrinting all Objects in List\n");
+	log_manager.writeLog("\tPrinting all Objects in List\n");
 	for (test_li.first(); !test_li.isDone(); test_li.next()) {
-		l_mngr.writeLog("\tObject type: %s, isDone(): %d\n", test_li.currentObject()->getType().c_str(), test_li.isDone());
+		log_manager.writeLog("\tObject type: %s, isDone(): %d\n", test_li.currentObject()->getType().c_str(), test_li.isDone());
 	}
 }
 
-void testWorldManager()
+void test_world_manager()
 {
 	/*
 	WorldManager Tests
 	 - Creates and checks object lists of world manager before and after step.
 	 */
-	l_mngr.writeLog("\n\n - WorldManager Test Suite - \n");
+	log_manager.writeLog("\n\n - WorldManager Test Suite - \n");
 	df::WorldManager &world_manager = df::WorldManager::getInstance();
-	l_mngr.writeLog("\t%s instance fetched.\n", world_manager.getType().c_str());
+	log_manager.writeLog("\t%s instance fetched.\n", world_manager.getType().c_str());
 
 	test_list = &world_manager.getAllObjects();
-	l_mngr.writeLog("\tWorldManager Object List getCount(): %d, isEmpty: %d\n", test_list->getCount(), test_list->isEmpty());
-	world_manager.removeObject(testObj);
-	world_manager.markForDelete(testObj2);
+	log_manager.writeLog("\tWorldManager Object List getCount(): %d, isEmpty: %d\n", test_list->getCount(), test_list->isEmpty());
+	world_manager.removeObject(test_obj);
+	world_manager.markForDelete(test_obj2);
 	world_manager.update();
 	test_list = &world_manager.getAllObjects();
-	l_mngr.writeLog("\tWorldManager Object List getCount(): %d, isEmpty: %d\n", test_list->getCount(), test_list->isEmpty());
-	l_mngr.setFlush(true);
+	log_manager.writeLog("\tWorldManager Object List getCount(): %d, isEmpty: %d\n", test_list->getCount(), test_list->isEmpty());
+	log_manager.setFlush(true);
 }
 
-void testGameManager()
+void test_game_manager()
 {
 	/*
 	GameManager Tests
 	 - Runs GameManager
 	 */
-	l_mngr.writeLog("\n\n - GameManager Test Suite - \n");
+	log_manager.writeLog("\n\n - GameManager Test Suite - \n");
 	df::Object *g_test = new df::Object();
-	g_mngr.run();
+	game_manager.run();
 }
